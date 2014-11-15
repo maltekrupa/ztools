8921f2bc865a67b4f516f761bbe32a639243aafb
David Adrian <davidcadrian@gmail.com>
2014-11-15 18:38:23 -0500

    Make Processs() blocking

--- processing/input.go
+++ processing/input.go
@@ -15,10 +15,13 @@ type Worker func(v interface{}) interface{}
 func Process(in Decoder, out Encoder, w Worker, workers uint) {
 	processQueue := make(chan interface{}, workers*4)
 	outputQueue := make(chan interface{}, workers*4)
+	doneChan := make(chan int, workers)
 	// Start the output encoder
 	go func() {
-		result := <-outputQueue
-		out.Encode(result)
+		for result := range outputQueue {
+			out.Encode(result)
+		}
+		doneChan <- 1
 	}()
 	// Start all the workers
 	for i := uint(0); i < workers; i++ {
@@ -27,6 +30,7 @@ func Process(in Decoder, out Encoder, w Worker, workers uint) {
 				result := w(obj)
 				outputQueue <- result
 			}
+			doneChan <- 1
 		}()
 	}
 	// Read the input, send to workers
@@ -37,4 +41,9 @@ func Process(in Decoder, out Encoder, w Worker, workers uint) {
 		}
 		processQueue <- obj
 	}
+	close(processQueue)
+	for i := uint(1); i < workers; i++ {
+		<-doneChan
+	}
+	<-doneChan
 }
